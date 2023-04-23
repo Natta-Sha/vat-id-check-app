a03a7a63789f71d80341c02d2263b54799579b68
Natalia Bohdanova <65080131+Natta-Sha@users.noreply.github.com>
2023-04-23 22:24:20 +0200

    added api integration

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import React from "react";
 import "./App.css";
+import VatIdCheck from "./VatIdCheck";
 
 export default function App() {
-  return <div className="App">Hi</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <VatIdCheck />
+        <footer className="App-footer">Coded by NB</footer>
+      </div>
+    </div>
+  );
 }
---
+++ src/VatIdCheck.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./VatIdCheck.css";
+
+//Documentation: https://vatcheckapi.com/docs/#authentication-methods
+
+export default function VatIdCheck() {
+  let [keyword, setKeyword] = useState("");
+
+  function handleResponse(response) {
+    console.log(response.data.registration_info.address);
+  }
+
+  function search(event) {
+    event.preventDefault();
+
+    let apiKey = `eg9uTHp9geZ0NlaT7CXL0jpPeBSNYXsvMQP0eLGa`;
+    let apiUrl = `https://api.vatcheckapi.com/v2/check?vat_number=EE101992839&apikey=${apiKey}`;
+
+    axios.get(apiUrl).then(handleResponse);
+  }
+  function handleKeywordChange() {
+    setKeyword(event.target.value);
+  }
+  return (
+    <div className="VatIdCheck">
+      <form onSubmit={search}>
+        <input
+          type="search"
+          placeholder="AB123456789 (Type VAT number including country code"
+          autoFocus={true}
+          onChange={handleKeywordChange}
+        />
+      </form>
+    </div>
+  );
+}
